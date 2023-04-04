b8a57a78282e4cade56b53f9048e9de91f705f64
yoshiboo <yoshikikaitech1414@gmail.com>
2023-04-04 12:36:48 +0900

    ログアウト機能のコンポーネントを追加

--- resources/views/components/tweet/form/container.blade.php
+++ resources/views/components/tweet/form/container.blade.php
@@ -1,5 +1,15 @@
 <div class="flex justify-center">
     <div class="max-w-screen-sm w-full">
+            <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <div class="flex justify-end p-4">
+                    <button 
+                        class="mt-2 text-sm text-gray-500 hover:text-gray-800"
+                        onClick="event.preventDefault(); this.closest('form').submit();"
+                    >ログアウト</button>
+                </div>
+            </form>
+
         {{ $slot }}
     </div>
 </div>
